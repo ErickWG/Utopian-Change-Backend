5f7c22f98ebb89bebce6dbb1610d65442647216c
Gincarlo <gianrodri2.2@gmail.com>
2023-09-16 21:04:21 -0500

    Agregue una Nota

--- src/main/java/com/upc/practice/Service/EmpresaService.java
+++ src/main/java/com/upc/practice/Service/EmpresaService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class EmpresaService {
+public class EmpresaService {//CambioGiancarlo
 
     @Autowired
     private final EmpresaRepo empresaRepo;
