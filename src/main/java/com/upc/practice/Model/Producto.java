e1fac86a0ac7fdd4516216abf5d0d5a8c35e1a5f
Miluzka <miluzka.vargas@gmail.com>
2023-09-20 20:01:07 -0500

    Quité "oo"

--- src/main/java/com/upc/practice/Model/Producto.java
+++ src/main/java/com/upc/practice/Model/Producto.java
@@ -16,7 +16,7 @@ import java.util.Date;
 public class Producto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idProducto;
+    private Long idProductoo;
     private String nombreProducto;
     private String detalleProducto;
     private double precioProducto;
