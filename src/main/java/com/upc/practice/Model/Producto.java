ce234c238be6a75587e4750cc0add98fb0d806c9
ViSlyX0104 <u202114496@upc.edu.pe>
2023-09-22 14:42:44 -0500

    Realicé la tabla "Productp_Material" mediante el Many To Many en la entidad Producto.

--- src/main/java/com/upc/practice/Model/Producto.java
+++ src/main/java/com/upc/practice/Model/Producto.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Data // Gets y Sets
@@ -24,4 +25,19 @@ public class Producto {
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
     private Empresa empresa;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name= "producto_material",
+            joinColumns = @JoinColumn(name = "id_producto", referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "id_material", referencedColumnName = "id"))
+    private List<Material> materials;
+
+    public Producto(String nombreProducto, String detalleProducto, double precioProducto, LocalDate fecha, Empresa empresa, List<Material> materials) {
+        this.nombreProducto = nombreProducto;
+        this.detalleProducto = detalleProducto;
+        this.precioProducto = precioProducto;
+        this.fecha = fecha;
+        this.empresa = empresa;
+        this.materials = materials;
+    }
 }
